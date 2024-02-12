5776b7ee0e9935a95f3e31bd1c7daba6f7c709d6
Liliana <ingsandovalpineda@gmail.com>
2024-02-12 16:54:43 -0500

    Primer commit GitJaca

---
+++ Prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author ingsa
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Hello World desde escritorio- !");
+        System.out.println("Esta es otra linea");
+        System.out.println("Segunda linea- !");
+    }
+}
